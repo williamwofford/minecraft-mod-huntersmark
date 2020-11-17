b1f74ec399321f24cd4a5f0bf23d37ebfe45224d
Borkleby <liamwofford@gmail.com>
2020-11-17 16:01:16 -0600

    initialized HunterManager, added class LocationMemory

--- src/us/williamwofford/huntersmark/HunterManager.java
+++ src/us/williamwofford/huntersmark/HunterManager.java
@@ -1,5 +1,63 @@
 package us.williamwofford.huntersmark;
 
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class HunterManager {
-    
+    private final Map< Player, Player > playerRelations = new HashMap<>();
+    private final Map< Player, LocationMemory > playerLocations = new HashMap<>();
+
+    public boolean isPlayerHunter( Player player ) {
+        return false;
+    }
+
+    public Location getCompassPointLocation( Player hunter, Player target ) throws UnnavigableError {
+        return null;
+    }
+
+    public void addHunter( Player player ) {
+
+    }
+
+    public void removeHunter( Player player ) {
+
+    }
+
+    public void setHunterTarget( Player hunter, Player target ) {
+
+    }
+
+    public void clearHunterTarget( Player hunter ) {
+
+    }
+
+    public void refreshHunterCompass( Player hunter ) {
+
+    }
+
+    public void setPlayerDimension( Player player, World.Environment dimension, Location location ) {
+
+    }
+
+    private class UnnavigableError extends Exception {
+        public UnnavigableError() {
+            super();
+        }
+
+        public UnnavigableError( String message ) {
+            super( message );
+        }
+
+        public UnnavigableError( String message, Throwable cause ) {
+            super( message, cause );
+        }
+
+        public UnnavigableError( Throwable cause ) {
+            super( cause );
+        }
+    }
 }
---
+++ src/us/williamwofford/huntersmark/LocationMemory.java
@@ -0,0 +1,5 @@
+package us.williamwofford.huntersmark;
+
+public class LocationMemory {
+
+}
