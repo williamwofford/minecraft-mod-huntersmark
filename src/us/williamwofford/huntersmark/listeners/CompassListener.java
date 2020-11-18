727ee0d50a60d38dcf2ed4571d36ad007d38426d
Borkleby <liamwofford@gmail.com>
2020-11-17 20:49:24 -0600

    fleshed: CompassListener

--- src/us/williamwofford/huntersmark/listeners/CompassListener.java
+++ src/us/williamwofford/huntersmark/listeners/CompassListener.java
@@ -1,7 +1,28 @@
 package us.williamwofford.huntersmark.listeners;
 
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import us.williamwofford.huntersmark.HuntersMark;
 
 public class CompassListener implements Listener {
 
+    @EventHandler
+    public void onInteract( PlayerInteractEvent e ) {
+        Player player = e.getPlayer();
+        Action action = e.getAction();
+
+        if ( HuntersMark.hunterManager.isPlayerHunter( player ) ) {
+
+            if ( action.equals( Action.RIGHT_CLICK_BLOCK ) || action.equals( Action.RIGHT_CLICK_AIR ) ) {
+
+                if ( player.getInventory().getItemInMainHand().getType() == Material.COMPASS ) {
+                    HuntersMark.hunterManager.refreshHunterCompass( player );
+                }
+            }
+        }
+    }
 }
