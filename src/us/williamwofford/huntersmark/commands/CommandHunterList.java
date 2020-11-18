81515680095f6fce2ecffa7551c6cd67a1a42519
Borkleby <liamwofford@gmail.com>
2020-11-17 20:35:45 -0600

    CommandHunterList: modified broadcast message

--- src/us/williamwofford/huntersmark/commands/CommandHunterList.java
+++ src/us/williamwofford/huntersmark/commands/CommandHunterList.java
@@ -11,12 +11,18 @@ public class CommandHunterList implements CommandExecutor {
 
     @Override
     public boolean onCommand( CommandSender commandSender, Command command, String s, String[] strings ) {
+        String result;
 
-        String result = "Current Hunters: \n";
-        for ( Player player : HuntersMark.hunterManager.getOnlineHunters() ) {
-            final ChatColor onlineStatusColor = player.isOnline() ? ChatColor.RESET : ChatColor.DARK_GRAY;
-            result += onlineStatusColor + player.getDisplayName() + " ";
+        if ( HuntersMark.hunterManager.getOnlineHunters().size() > 0) {
+            result = "Current Hunters online: ";
+            for ( Player player : HuntersMark.hunterManager.getOnlineHunters() ) {
+                final ChatColor onlineStatusColor = player.isOnline() ? ChatColor.RESET : ChatColor.DARK_GRAY;
+                result += onlineStatusColor + player.getDisplayName() + " ";
+            }
+        } else {
+            result = "There are currently no Hunters online.";
         }
+
         commandSender.sendMessage( result );
 
         return true;
