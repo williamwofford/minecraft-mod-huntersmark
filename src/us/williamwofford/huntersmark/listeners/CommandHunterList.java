cef316fc023162d749885e2467be8cd239e8db57
Borkleby <liamwofford@gmail.com>
2020-11-17 18:12:35 -0600

    added: CommandHunterList

---
+++ src/us/williamwofford/huntersmark/listeners/CommandHunterList.java
@@ -0,0 +1,13 @@
+package us.williamwofford.huntersmark.listeners;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandHunterList implements CommandExecutor {
+
+    @Override
+    public boolean onCommand( CommandSender commandSender, Command command, String s, String[] strings ) {
+        return false;
+    }
+}
