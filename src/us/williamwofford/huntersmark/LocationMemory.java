cfbdb233d02f0fe151088f445d84f6c3281ba8c7
Borkleby <liamwofford@gmail.com>
2020-11-17 18:07:22 -0600

    initialized LocationMemory

--- src/us/williamwofford/huntersmark/LocationMemory.java
+++ src/us/williamwofford/huntersmark/LocationMemory.java
@@ -1,5 +1,27 @@
 package us.williamwofford.huntersmark;
 
-public class LocationMemory {
+import org.bukkit.Location;
+import org.bukkit.World;
 
+public class LocationMemory {
+    private Location overworld;
+    private Location nether;
+    private Location end;
+
+    public LocationMemory( Location overworld, Location nether, Location end ) {
+        this.overworld = overworld;
+        this.nether = nether;
+        this.end = end;
+    }
+    public LocationMemory() {
+        this( null, null, null );
+    }
+
+    public Location getLocation( World.Environment dimension ) throws Exception {
+        return null;
+    }
+
+    public void setLocation( World.Environment dimension, Location location ) {
+
+    }
 }
