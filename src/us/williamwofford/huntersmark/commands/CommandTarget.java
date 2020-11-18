07b11769c097c014481785d2ae84eb9662c81cd9
Borkleby <liamwofford@gmail.com>
2020-11-17 19:30:56 -0600

    fleshed: CommandTarget

--- src/us/williamwofford/huntersmark/commands/CommandTarget.java
+++ src/us/williamwofford/huntersmark/commands/CommandTarget.java
@@ -1,13 +1,29 @@
 package us.williamwofford.huntersmark.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import us.williamwofford.huntersmark.HuntersMark;
 
 public class CommandTarget implements CommandExecutor {
 
     @Override
     public boolean onCommand( CommandSender commandSender, Command command, String s, String[] strings ) {
+        if ( commandSender instanceof Player ) {
+            final Player hunter = (Player) commandSender;
+
+            if ( HuntersMark.hunterManager.isPlayerHunter( hunter ) ) {
+                final Player target = Bukkit.getServer().getPlayer( strings[0] );
+
+                if ( target != null ) {
+                    HuntersMark.hunterManager.setHunterTarget( hunter, target );
+
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
