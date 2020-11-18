11e951f63c9c5f80cd175e87ad9414fa138bdb51
Borkleby <liamwofford@gmail.com>
2020-11-17 18:13:55 -0600

    added: CommandTarget

---
+++ src/us/williamwofford/huntersmark/listeners/CommandTarget.java
@@ -0,0 +1,13 @@
+package us.williamwofford.huntersmark.listeners;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandTarget implements CommandExecutor {
+
+    @Override
+    public boolean onCommand( CommandSender commandSender, Command command, String s, String[] strings ) {
+        return false;
+    }
+}
